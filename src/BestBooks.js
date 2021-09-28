bbdd2ecc46cece9f50b95cd05f27104ceee8d3c2
Bryce Pfingston <bryce.pfingston@gmail.com>
2021-09-28 00:34:10 -0600

    put in carrousel, profile in the nav, and rendered words in teh carrousel

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,31 +1,60 @@
-import React from 'react';
+import axios from "axios";
+import { Component } from "react";
+import Carousel from "react-bootstrap/Carousel";
 
-class BestBooks extends React.Component {
+export default class BestBooks extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      books: []
-    }
+      books:[],
+      bookAmount: '',
+
+    };
+
   }
 
-  /* TODO: Make a GET request to your API to fetch books for the logged in user  */
+  componentDidMount() {
+    this.getBooks();
+  }
+
+async getBooks() {
+  let apiURL = 'http://localhost:3003/books';
+
+  try {
+    const response = await axios.get (apiURL);
+    const bookData = response.data;
+    if(bookData.length > 0) {
+    this.setState({ books: bookData});
+  } else {
+    this.setState({ bookAmount: 'All the books are gone' });
+  }} catch (error) {
+    console.log('ErRor 404');
+  }
+}
 
   render() {
 
-    /* TODO: render user's books in a Carousel */
+    
 
     return (
       <>
         <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
-
-        {this.state.books.length ? (
-          <p>Book Carousel coming soon</p>
-        ) : (
-          <h3>No Books Found :(</h3>
+        <Carousel>
+         
+         {this.state.books.map((bookdata, index) => 
+         <Carousel.Item key={index}> 
+          <h1>{bookdata.Title}</h1>  
+          <h4>{bookdata.description}
+          {bookdata.email}
+          {bookdata.status} </h4>
+          
+        </Carousel.Item>
         )}
+        </Carousel>
+
+        {this.state.emptyMessage && <h1>{this.state.emptyMessage}</h1> }
       </>
     )
   }
 }
 
-export default BestBooks;
