639e390ea2fc6d5ed9517314e3610d780a7edaf6
Bryce Pfingston <bryce.pfingston@gmail.com>
2021-09-30 16:58:49 -0600

    log on and off works, profile works, and delete works

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -4,20 +4,28 @@ import Button from 'react-bootstrap/Button'
 
 export default class LoginForm extends Component {
 
+  userInfo = (event) => {
+    event.preventDefault();
+      var user = { 
+        UserName: event.target.userName.value,
+        Email: event.target.email.value,
+      };
+      this.props.loginHandler(user);
+  }
   render() {
     /* TODO: create a simple login form that collects username and and email, and lets parent component know when form has been submitted */
     return (
-      <Form>
-        <Form.Group className="mb-3" controlId="formUserName">
+      <Form onSubmit={this.userInfo}>
+        <Form.Group className="mb-3" controlId="userName">
           <Form.Label>Username</Form.Label>
           <Form.Control type="text" placeholder="Please enter a valid Username"/>
         </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formEmail">
+        <Form.Group className="mb-3" controlId="email">
           <Form.Label>Email</Form.Label>
           <Form.Control type="text" placeholder="Please enter a valid Email Address"/>
         </Form.Group>
-        <Button onClick={event => this.props.loginHandler(event)} type="submit" variant="primary">Submit</Button>
+        <Button type="submit" variant="primary">Submit</Button>
       </Form>
     );
   }
